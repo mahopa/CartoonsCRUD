7694350863e79ed7005ec80d4eace24b16e12127
Mahopa <mahopa@gmail.com>
2014-07-02 14:36:49 -0500

    Added Routing and views and filters

--- AgainCartoons/Scripts/_myJS.js
+++ AgainCartoons/Scripts/_myJS.js
@@ -1,7 +1,32 @@
-﻿var app = angular.module("Cartoons", []);
+﻿var app = angular.module("Cartoons", ["ngRoute"]);
 app.factory("CartoonsFactory", function () {
-    return [{name:"Invader Zim", year:"The Future"}, {name:"Foghorn Leghorn", year:"Ante-bellum"}];
-})
+    return [
+        { name: "Invader Zim", year: "The Future" },
+        { name: "Foghorn Leghorn", year: "Ante-bellum" },
+        { name: "Thunder Cats", year: "80s" },
+        { name: "Futurama", year: "3000" },
+        { name: "Harvey Birdman: Attorney at Law", year: "2013" },
+        { name: "Sealab: 2021", year: "2021" },
+        { name: "Aqua-teen Hunger Force", year: "2008" },
+        { name: "Mission Hill", year: "2021" }
+    ];
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "Views/home.html",
+        controller:"AddController"
+    });
+    $routeProvider.when("/Details", {
+        template: "This is the details page",
+         controller:"AddController"
+    })
+    $routeProvider.otherwise({
+        templateUrl: "Views/error.html",
+        controller: "ErrorController"
+    });
+});
+
 app.controller("AddController", function ($scope, CartoonsFactory) {
     $scope.cartoons = CartoonsFactory;
     $scope.AddCartoon = function (neCartoon) {
