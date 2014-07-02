2bb03aa98ddb88b5265b5e2b673c1383bb76aced
Mahopa <mahopa@gmail.com>
2014-07-02 12:01:08 -0500

    Initial

---
+++ AgainCartoons/Scripts/_myJS.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module("Cartoons", []);
+app.factory("CartoonsFactory", function () {
+    return [{name:"Invader Zim", year:"The Future"}, {name:"Foghorn Leghorn", year:"Ante-bellum"}];
+})
+app.controller("AddController", function ($scope, CartoonsFactory) {
+    $scope.cartoons = CartoonsFactory;
+    $scope.AddCartoon = function (neCartoon) {
+        var c =  {name: neCartoon.name, year: neCartoon.year};
+        $scope.cartoons.push(c);
+        $scope.newCartoon = "";
+    };
+    $scope.DeleteCartoon = function (object) {
+        $scope.cartoons.splice(
+            $scope.cartoons.indexOf(object),
+            1);
+    };
+});
