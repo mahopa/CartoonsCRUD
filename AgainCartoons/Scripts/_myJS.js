ea77b00866d93a41037d2f6bf6b014e35a2831d9
Mahopa <mahopa@gmail.com>
2014-07-03 13:52:07 -0500

    Added $http post, get delete calls

--- AgainCartoons/Scripts/_myJS.js
+++ AgainCartoons/Scripts/_myJS.js
@@ -1,14 +1,14 @@
 ﻿var app = angular.module("Cartoons", ["ngRoute"]);
 app.factory("CartoonsFactory", function () {
     return [
-        { name: "Invader Zim", year: "The Future" },
-        { name: "Foghorn Leghorn", year: "Ante-bellum" },
-        { name: "Thunder Cats", year: "80s" },
-        { name: "Futurama", year: "3000" },
-        { name: "Harvey Birdman: Attorney at Law", year: "2013" },
-        { name: "Sealab: 2021", year: "2021" },
-        { name: "Aqua-teen Hunger Force", year: "2008" },
-        { name: "Mission Hill", year: "2021" }
+        //{ name: "Invader Zim", year: "The Future" },
+        //{ name: "Foghorn Leghorn", year: "Ante-bellum" },
+        //{ name: "Thunder Cats", year: "80s" },
+        //{ name: "Futurama", year: "3000" },
+        //{ name: "Harvey Birdman: Attorney at Law", year: "2013" },
+        //{ name: "Sealab: 2021", year: "2021" },
+        //{ name: "Aqua-teen Hunger Force", year: "2008" },
+        //{ name: "Mission Hill", year: "2021" }
     ];
 });
 
@@ -27,16 +27,44 @@ app.config(function ($routeProvider) {
     });
 });
 
-app.controller("AddController", function ($scope, CartoonsFactory) {
+app.controller("AddController", function ($scope, CartoonsFactory, $http) {
     $scope.cartoons = CartoonsFactory;
+    $scope.getCartoons = function () {
+        if ($scope.cartoons.length) { }
+        else {
+            $http.get("https://domo.firebaseio.com/.json")
+                  .success(function (data, status, headers, config) {
+                      for (var x in data) {
+                          data[x].key = x;
+                          $scope.cartoons.push(data[x]);
+                      }
+                  })
+                  .error(function () { })
+
+        }
+    };
     $scope.AddCartoon = function (neCartoon) {
-        var c =  {name: neCartoon.name, year: neCartoon.year};
-        $scope.cartoons.push(c);
-        $scope.newCartoon = "";
+        var c = { name: neCartoon.name, year: neCartoon.year };
+        $http.post("https://domo.firebaseio.com/.json", c)
+             .success(function (data) {
+                 console.log("Response: "+JSON.stringify(data));
+                 c.key = data.name;
+                 console.log(data.name);
+                 console.log(JSON.stringify(c));
+                 $scope.cartoons.push(c);
+                 $scope.newCartoon = "";
+             })
+             .error(function () { })
     };
     $scope.DeleteCartoon = function (object) {
-        $scope.cartoons.splice(
-            $scope.cartoons.indexOf(object),
-            1);
+
+        $http.delete("https://domo.firebaseio.com/" + object.key + "/.json")
+        .success(function () {
+            var targetIndex =$scope.cartoons.indexOf(object);
+            $scope.cartoons.splice(targetIndex, 1);
+        })
+        .error(function () { });
+        
     };
+    $scope.getCartoons();
 });
